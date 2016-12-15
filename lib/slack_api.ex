e81be8311d4490892bb017c078ba27e93bd9ab69
Ryan Weald <ryan@weald.com>
2016-12-15 09:37:18 -0800

    Don't include slack token in public repo. Whoops

--- lib/slack_api.ex
+++ lib/slack_api.ex
@@ -2,7 +2,7 @@ defmodule FitLeaderboard.SlackAPI do
   use HTTPoison.Base
 
   def process_url(url) do
-    "https://slack.com/api/" <> url <> "?token=xoxp-3053565023-3053663269-116390157744-77ab1313bef1c236014e9e2097a2272e"
+    "https://slack.com/api/" <> url <> "?token=" <> System.get_env("SLACK_TOKEN")
   end
 
   def process_response_body(body) do
